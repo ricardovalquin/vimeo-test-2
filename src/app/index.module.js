add491bd0722c0b8d76dfecbbc12c2b20e44249c
Ricardo Valencia <ricardovaquin@gmail.com>
2016-06-01 09:29:53 -0500

    generated project

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('vimeoTest2', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
